991d9f101d5c5eb58add00336b7a4ac7db322691
KerithWiston96 <kerithwiston1996@gmail.com>
2024-10-22 08:29:27 +0000

    Initial commit

---
+++ app/Models/Statistiquetrafic.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Statistiquetrafic extends Model
+{
+    use HasFactory;
+}
