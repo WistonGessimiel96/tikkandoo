a6cb00e4079ac86e85efefde4483740d783746ad
Dany-Whyyou <daniel.doviakon@gmail.com>
2024-10-24 17:24:40 +0000

    integration initiale admin lte

---
+++ config/menu.php
@@ -0,0 +1,141 @@
+<?php
+
+return [
+    "menu" => [
+        [
+            "headLib" => "dashboard",
+            "headActivate" => "dashboardActivate",
+            "headOpen" => "dashboardOpen",
+            "content" => [
+                [
+                    "libelle" => "Tableau de bord",
+                    "active" => "Tableau de bord",
+                    "niveau" => 1,
+                ],
+                [
+                    "libelle" => "Statistiques",
+                    "active" => "Statistiques",
+                    "niveau" => 1,
+                ],
+                [
+                    "libelle" => "Graph",
+                    "active" => "Graph",
+                    "niveau" => 1,
+                ],
+            ],
+        ],
+        [
+            "headLib" => "utilisateurs",
+            "headActivate" => "utilisateursActivate",
+            "headOpen" => "utilisateursOpen",
+            "content" => [
+                [
+                    "libelle" => "Gestion des clients",
+                    "active" => "Gestion des clients",
+                    "niveau" => 1,
+                ],
+                [
+                    "libelle" => "Gestion des agents",
+                    "active" => "Gestion des agents",
+                    "niveau" => 1,
+                ],
+                [
+                    "libelle" => "Gestion des administrateurs",
+                    "active" => "Gestion des administrateurs",
+                    "niveau" => 1,
+                ],
+                [
+                    "libelle" => "Types d'utilisateur",
+                    "active" => "Types d'utilisateur",
+                    "niveau" => 1,
+                ],
+            ],
+        ],
+        [
+            "headLib" => "codes",
+            "headActivate" => "codesActivate",
+            "headOpen" => "codesOpen",
+            "content" => [
+                [
+                    "libelle" => "Tous les codes",
+                    "active" => "Tous les codes",
+                    "niveau" => 1,
+                ],
+                [
+                    "libelle" => "Codes controlés",
+                    "active" => "Codes controlés",
+                    "niveau" => 1,
+                ],
+                [
+                    "libelle" => "Codes précommandés",
+                    "active" => "Codes précommandés",
+                    "niveau" => 1,
+                ],
+                [
+                    "libelle" => "Codes utilisés",
+                    "active" => "Codes utilisés",
+                    "niveau" => 1,
+                ],
+            ]
+        ],
+        [
+            "headLib" => "forfaits",
+            "headActivate" => "forfaitsActivate",
+            "headOpen" => "forfaitsOpen",
+            "content" => [
+                [
+                    "libelle" => "Tarifs de ticket",
+                    "active" => "Tarifs de ticket",
+                    "niveau" => 1,
+                ],
+                [
+                    "libelle" => "Abonnements",
+                    "active" => "Abonnements",
+                    "niveau" => 1,
+                ],
+                [
+                    "libelle" => "Client Abonnés",
+                    "active" => "Client Abonnés",
+                    "niveau" => 1,
+                ],
+                [
+                    "libelle" => "Historique des achats (Ticket)",
+                    "active" => "Historique des achats (Ticket)",
+                    "niveau" => 1,
+                ],
+                [
+                    "libelle" => "Historique des abonnements",
+                    "active" => "Historique des abonnements",
+                    "niveau" => 1,
+                ],
+            ]
+        ],
+        [
+            "headLib" => "transactions",
+            "headActivate" => "transactionsActivate",
+            "headOpen" => "transactionsOpen",
+            "content" => [
+                [
+                    "libelle" => "Historique des transactions",
+                    "active" => "Historique des transactions",
+                    "niveau" => 1,
+                ],
+                [
+                    "libelle" => "Transactions réussit",
+                    "active" => "Transactions réussit",
+                    "niveau" => 1,
+                ],
+                [
+                    "libelle" => "Transactions échouées",
+                    "active" => "Transactions échouées",
+                    "niveau" => 1,
+                ],
+                [
+                    "libelle" => "Transactions en attentes",
+                    "active" => "Transactions en attentes",
+                    "niveau" => 1,
+                ],
+            ]
+        ],
+    ]
+];
